453dd91c0ef06fcb386b26952b72a25bb0167458
Ryan Tan <rtan265@aucklanduni.ac.nz>
2021-03-25 17:28:32 +1300

    Solved API call function, and added change URL feature to workon goals instead

--- background.js
+++ background.js
@@ -1,18 +1,24 @@
 
+const newURL = "https://trello.com/b/W2wIXDdD/goals-2021";
+
 chrome.runtime.onInstalled.addListener(function() {
   console.log("Installed on updated or new browsers");
 });
 
-const urlFilters = [{urlMatches: 'https://www.facebook.com/*'}, {urlMatches: 'https://www.linkedin.com/*'}];
+const urlFilters = [{urlMatches: 'https://www.facebook.com/*'}, {urlMatches: 'https://www.linkedin.com/*'}, {urlMatches: 'https://www.instagram.com/'}];
 chrome.webNavigation.onCompleted.addListener(async function
   (details) {
-    // Get current time, count hours till 7pm
-    // if >7pm and < 7am allow, else close
-    var result;
-    await fetch("http://worldtimeapi.org/api/ip")
+    let result;
+    const data = await fetch("http://worldtimeapi.org/api/ip")
       .then(response => result = response.json())
-      .then(console.log)
 
-    alert("You're not supposed to be on social media at this hour! Closing this tab.");
-    chrome.tabs.remove(details.tabId);
+    let dateTime = new Date(data.datetime)
+    let hour = dateTime.getHours()
+
+    if (hour > 7 && hour < 19){
+      const remainder = 19 - hour;
+      alert("You're not supposed to be on social media at this hour! Closing this tab. You have another " + remainder + " hours to go. You could work on your goals instead!");
+      chrome.tabs.update(details.tabId, { url: newURL });
+    }
+
 }, {url: urlFilters});
