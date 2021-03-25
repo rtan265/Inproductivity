cf0345d29289f7cdfecfbb118a7cf8e7d1bdce2c
Ryan Tan <rtan265@aucklanduni.ac.nz>
2021-03-25 17:05:52 +1300

    fetch now working

--- background.js
+++ background.js
@@ -4,19 +4,15 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 const urlFilters = [{urlMatches: 'https://www.facebook.com/*'}, {urlMatches: 'https://www.linkedin.com/*'}];
-chrome.webNavigation.onCompleted.addListener(function
+chrome.webNavigation.onCompleted.addListener(async function
   (details) {
     // Get current time, count hours till 7pm
     // if >7pm and < 7am allow, else close
+    var result;
+    await fetch("http://worldtimeapi.org/api/ip")
+      .then(response => result = response.json())
+      .then(console.log)
 
     alert("You're not supposed to be on social media at this hour! Closing this tab.");
     chrome.tabs.remove(details.tabId);
 }, {url: urlFilters});
-
-async function getNZTime(){
-  var result;
-  await fetch("http://worldtimeapi.org/api/ip")
-    .then(response => result = response.json())
-    .then(data => console.log(data))
-  return result;
-}
