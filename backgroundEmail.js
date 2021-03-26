e143e8dd0f2a7958d87f527d6d190e8ea7978c78
Ryan Tan <rtan265@aucklanduni.ac.nz>
2021-03-26 19:03:33 +1300

    Added default value function for counter

--- backgroundEmail.js
+++ backgroundEmail.js
@@ -1,19 +1,30 @@
 const mailUrlFilters = [{urlMatches: 'https://mail.*'}];
+const STORAGE = chrome.storage.local;
 
 chrome.runtime.onInstalled.addListener(function() {
+  STORAGE.clear(function() {
+    var error = chrome.runtime.lastError;
+    if (error){
+      console.error(error);
+    }
+  });
+
+  console.log("Hello");
+  var data = {};
+  data["counter"] = 0;
+
+  STORAGE.set(data, function(){
+  })
+
 });
 
-chrome.webNavigation.onCompleted.addListener(async function(details) {
-  chrome.storage.sync.get(['counter'], function(result) {
-    console.log('value: ' + result.counter)
-    if (result.counter == undefined) {
-      chrome.storage.sync.set({"counter": 1}, function() {
-      })
-    }
-  })
 
-  chrome.storage.sync.get(['counter'], function(result) {
-    console.log('value @ 2: ' + result.counter)
+chrome.webNavigation.onCompleted.addListener(async function(details) {
+  // we want to create functions to get and set responses so that it's asynchronous
+  let currentCounter;
+  STORAGE.get("counter", function(result){
+    currentCounter = result + 1;
+    STORAGE.set({"counter": currentCounter}, function() {
+    })
   })
-  
 }, {url: mailUrlFilters});
