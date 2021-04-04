9b4eebd0958175a798f880120cd171399397fea6
Ryan Tan <rtan265@aucklanduni.ac.nz>
2021-04-05 00:42:21 +1200

    Added button for popup.html

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var link = document.getElementById('trelloLink');
+  // onClick's logic below:
+  link.addEventListener('click', function() {
+      alert("Hello");
+  });
+});
